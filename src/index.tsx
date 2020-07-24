bd7c971359aba0b212c6c34351753b4376ad6f42
ziad <ziad.alsabouni@outlook.com>
2020-07-24 22:28:26 +0300

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,48 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'font-awesome/scss/font-awesome.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from "redux";
+
+
+//actions
+const increment = () => {
+    return {
+        type: "INCREMENT",
+    }
+}
+
+const decrement = () => {
+    return {
+        type: "DECREMENT",
+    }
+}
+
+//reducers
+const counter = (state = 0 ,action)=>{
+    switch (action.type) {
+        case "INCREMENT":
+            return state +1
+        case "DECREMENT":
+            return state - 1
+    }
+}
+
+//store
+let store = createStore(counter);
+
+store.subscribe(()=>{ console.log(store.getState())})
+
+//dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
