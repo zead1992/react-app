5d835f5f2417cf7fb6acfac58cea987bb504ead8
ziad <ziad.alsabouni@outlook.com>
2020-07-25 13:33:48 +0300

    redux: async actions

--- src/store/reducers/movieReducer.ts
+++ src/store/reducers/movieReducer.ts
@@ -1,19 +1,32 @@
 import {IMovie} from "../../types/movie-types";
 
-const initState : {movies:IMovie[],loading:boolean,error:string} = {
-    movies:[],
-    loading:false,
-    error:''
+const initState: { movies: IMovie[], loading: boolean, error: string } = {
+    movies: [],
+    loading: false,
+    error: ''
 }
 
 export const movieReducer = (state = initState, action) => {
     switch (action.type) {
         case "FETCH_MOVIES":
-            return state;
+            return {
+                ...state,
+                loading: true
+            };
         case "FETCH_MOVIES_SUCCESS":
-            return state;
+            return {
+                ...state,
+                loading: false,
+                movies: action.payload,
+                error: ''
+            };
         case "FETCH_MOVIES_FAILURE":
-            return state;
+            return {
+                ...state,
+                loading: false,
+                movies: [],
+                error: action.payload
+            };
         default :
             return state
     }
