f82b800c2db870c55cdea730b128e08a16a8efca
ziad <ziad.alsabouni@outlook.com>
2020-08-07 03:07:22 +0300

    formik: component

--- src/components/YoutubeFormikForm.tsx
+++ src/components/YoutubeFormikForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FormikErrors, useFormik} from "formik";
+import {ErrorMessage, Field, Form, Formik, FormikErrors, useFormik} from "formik";
 import * as Yup from 'yup';
 
 type IForm = {
@@ -16,97 +16,105 @@ function YoutubeFormikForm(props) {
         email: Yup.string().email('invalid format').required('required'),
         channel: Yup.string().required('required')
     });
-    const formik = useFormik<IForm>({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-        isInitialValid: false,
-        validationSchema:formSchema,
-        // validate: (values) => {
-        //     let errors: FormikErrors<IForm> = {};
-        //
-        //     if (!values.name) {
-        //         errors.name = 'field is required'
-        //     }
-        //
-        //     if (!values.email) {
-        //         errors.email = 'field is required'
-        //     }
-        //
-        //     if (!values.channel) {
-        //         errors.channel = 'field is required'
-        //     }
-        //
-        //     return errors;
-        // },
-        onSubmit: (values) => {
-            console.log(values);
-        },
-
-    });
+    const initialValues: IForm = {
+        name: '',
+        email: '',
+        channel: ''
+    }
+    // const formik = useFormik<IForm>({
+    //     initialValues: {
+    //         name: '',
+    //         email: '',
+    //         channel: ''
+    //     },
+    //     isInitialValid: false,
+    //     validationSchema:formSchema,
+    //     // validate: (values) => {
+    //     //     let errors: FormikErrors<IForm> = {};
+    //     //
+    //     //     if (!values.name) {
+    //     //         errors.name = 'field is required'
+    //     //     }
+    //     //
+    //     //     if (!values.email) {
+    //     //         errors.email = 'field is required'
+    //     //     }
+    //     //
+    //     //     if (!values.channel) {
+    //     //         errors.channel = 'field is required'
+    //     //     }
+    //     //
+    //     //     return errors;
+    //     // },
+    //     onSubmit: (values) => {
+    //         console.log(values);
+    //     },
+    //
+    // });
 
     const formKeys = (key: keyof IForm) => {
         return key;
+    }
+
+    const onSubmit = (values: IForm) => {
+        console.log(values);
     }
 
 
     return (
         <div className="row">
             <div className="col-6">
-                <form onSubmit={formik.handleSubmit}>
-                    <h1>Formik Form</h1>
-                    <div className="form-group">
-                        <label htmlFor={formKeys('name')}>name</label>
-                        <input name={formKeys('name')}
-                               type="text"
-                               {...formik.getFieldProps(formKeys('name'))}
-                               className="form-control"
-                               id={formKeys('name')}/>
-                        {
-                            formik.touched.name && formik.errors.name &&
-                            <small className="text-danger">
-                                {formik.errors.name}
-                            </small>
-                        }
-
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor={formKeys('email')}>email</label>
-                        <input name={formKeys('email')}
-                               type="email"
-                               {...formik.getFieldProps(formKeys('email'))}
-                               className="form-control"
-                               id={formKeys('email')}/>
-                        {
-                            formik.touched.email && formik.errors.email &&
-                            <small className="text-danger">
-                                {formik.errors.email}
-                            </small>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor={formKeys('channel')}>channel</label>
-                        <input name={formKeys('channel')}
-                               type="text"
-                               {...formik.getFieldProps(formKeys('channel'))}
-                               className="form-control"
-                               id={formKeys('channel')}/>
-                        {
-                            formik.touched.channel && formik.errors.channel &&
-                            <small className="text-danger">
-                                {formik.errors.channel}
-                            </small>
-                        }
-                    </div>
-                    <button
-                        disabled={!formik.isValid}
-                        type="submit"
-                        className="btn btn-primary">
-                        Submit
-                    </button>
-                </form>
+                <Formik<IForm>
+                    initialValues={initialValues}
+                    isInitialValid={false}
+                    onSubmit={onSubmit}
+                    validationSchema={formSchema}
+                >
+                    {(props) => {
+                        return <Form>
+                            <h1>Formik Form</h1>
+                            <div className="form-group">
+                                <label htmlFor={formKeys('name')}>name</label>
+                                <Field name={formKeys('name')}
+                                       type="text"
+                                       className="form-control"
+                                       id={formKeys('name')}/>
+                                <ErrorMessage name={formKeys('name')}>
+                                    {errorMessage => <small className="text-danger">{errorMessage}</small>}
+                                </ErrorMessage>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor={formKeys('email')}>email</label>
+                                <Field name={formKeys('email')}
+                                       type="email"
+                                       className="form-control"
+                                       id={formKeys('email')}/>
+                                <ErrorMessage name={formKeys('email')}>
+                                    {errorMessage => <small className="text-danger">{errorMessage}</small>}
+                                </ErrorMessage>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor={formKeys('channel')}>channel</label>
+                                <Field name={formKeys('channel')}
+                                       type="text"
+                                    // value={formik.values.channel}
+                                    // onChange={formik.handleChange}
+                                    // onBlur={formik.handleBlur}
+                                       className="form-control"
+                                       id={formKeys('channel')}/>
+                                <ErrorMessage name={formKeys('channel')}>
+                                    {errorMessage => <small className="text-danger">{errorMessage}</small>}
+                                </ErrorMessage>
+                            </div>
+                            <button
+                                disabled={!props.isValid}
+                                type="submit"
+                                className="btn btn-primary">
+                                Submit
+                            </button>
+                        </Form>
+                    }}
+                </Formik>
             </div>
         </div>
     );
