49f70eb82c3f18f6f698fe2f7e7a271d382aeb9d
ziad <ziad.alsabouni@outlook.com>
2020-08-07 23:42:44 +0300

    formik: field as textarea

--- src/components/YoutubeFormikForm.tsx
+++ src/components/YoutubeFormikForm.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {ErrorMessage, Field, Form, Formik, FormikErrors, useFormik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 import * as Yup from 'yup';
 
 type IForm = {
     name: string;
     email: string;
     channel: string;
+    comment: string;
 }
 
 function YoutubeFormikForm(props) {
@@ -14,12 +15,14 @@ function YoutubeFormikForm(props) {
     const formSchema = Yup.object<IForm>({
         name: Yup.string().required('required'),
         email: Yup.string().email('invalid format').required('required'),
-        channel: Yup.string().required('required')
+        channel: Yup.string().required('required'),
+        comment: Yup.string().required('required').max(200, 'allowed string length 200')
     });
     const initialValues: IForm = {
         name: '',
         email: '',
-        channel: ''
+        channel: '',
+        comment: ''
     }
     // const formik = useFormik<IForm>({
     //     initialValues: {
@@ -103,6 +106,20 @@ function YoutubeFormikForm(props) {
                                        className="form-control"
                                        id={formKeys('channel')}/>
                                 <ErrorMessage name={formKeys('channel')}>
+                                    {errorMessage => <small className="text-danger">{errorMessage}</small>}
+                                </ErrorMessage>
+                            </div>
+                            {/*comment*/}
+                            <div className="form-group">
+                                <label htmlFor={formKeys('comment')}>comment</label>
+                                <Field
+                                    as='textarea'
+                                    name={formKeys('comment')}
+                                    type="text"
+                                    placeholder={'comment'}
+                                    className="form-control"
+                                    id={formKeys('comment')}/>
+                                <ErrorMessage name={formKeys('comment')}>
                                     {errorMessage => <small className="text-danger">{errorMessage}</small>}
                                 </ErrorMessage>
                             </div>
