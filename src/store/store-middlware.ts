32897026f9cffd2f0af4d91ff6fbe899f80d66b3
ziad <ziad.alsabouni@outlook.com>
2021-07-24 03:09:48 +0300

    genres slice

--- src/store/store-middlware.ts
+++ src/store/store-middlware.ts
@@ -1,7 +1,8 @@
 import {addMovieAsync, editMovieAsync} from "../features/movies/moviesSlice";
 import {updateLoadingState} from "../features/loading/loadingSlice";
+import {Action, ActionCreatorWithPayload, PayloadAction} from "@reduxjs/toolkit";
 
-export const actionListener =  storeAPI => next => action => {
+export const actionListener =  storeAPI => next => (action : PayloadAction) => {
     if(action.type == editMovieAsync.fulfilled.type || action.type == addMovieAsync.fulfilled.type){
         storeAPI.dispatch(updateLoadingState({key:'newMovie',val:false}));
     }
